a1475b7beec8f2fe15d2405fda7ea7cb12eaab95
Trung112277 <nhattrung112277@GMAIL.COM>
2025-05-15 09:05:57 +0700

    deploy github page

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 export default defineConfig(() => ({
   root: __dirname,
   cacheDir: './node_modules/.vite/todo-list-react-v2',
+  base: '/todo-list-react-v2/',
   server: {
     port: 4200,
     host: 'localhost',
